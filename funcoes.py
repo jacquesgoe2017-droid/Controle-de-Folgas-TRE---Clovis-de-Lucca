fed7857458e643793d8d0609b36819a6247dc792
jacquesgoe2017-droid <jacquesgoe2017@gmail.com>
2026-09-11 19:28:42 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -35,6 +35,7 @@ def gerar_pdf_certidao(nome, cpf, saldo, historico_creditos, nome_assinante, car
     styles = getSampleStyleSheet()
     
     style_t = ParagraphStyle('T', fontName='Helvetica-Bold', fontSize=13, leading=16, alignment=TA_CENTER)
+    style_e = ParagraphStyle('E', fontName='Helvetica', fontSize=10, leading=14, alignment=TA_CENTER)
     style_c = ParagraphStyle('C', fontName='Helvetica', fontSize=11, leading=17, alignment=TA_JUSTIFY)
     style_d = ParagraphStyle('D', fontName='Helvetica', fontSize=11, leading=16, alignment=TA_RIGHT)
     style_a = ParagraphStyle('A', fontName='Helvetica', fontSize=11, leading=16, alignment=TA_CENTER)
@@ -53,8 +54,8 @@ def gerar_pdf_certidao(nome, cpf, saldo, historico_creditos, nome_assinante, car
         Paragraph("<b>Secretaria de Estado da Educação</b>", style_t),
         Paragraph("<b>Unidade Regional de Ensino de São Bernardo do Campo</b>", style_t),
         Paragraph("<b>E.E. Clovis de Lucca</b>", style_t),
-        Paragraph("<b>Rua dos Vianas, 1915 - Baeta Neves - S.B. Campo - SP</b>", style_t),
-        Paragraph("<b>E-mail: e009124a@educacao.sp.gov.br - Fone: 11 - 4332-6372</b>", style_t),
+        Paragraph("<b>Rua dos Vianas, 1915 - Baeta Neves - S.B. Campo - SP</b>", style_e),
+        Paragraph("<b>E-mail: e009124a@educacao.sp.gov.br - Fone: 11 - 4332-6372</b>", style_e),,
         Spacer(1, 25),
         Paragraph("<u><b>CERTIDÃO DE LIQUIDAÇÃO DE FOLGAS - TRE</b></u>", style_t),
         Spacer(1, 30)
